57c554879403d0d9d7798ca86d28564cf42f2e23
SAGEN SAKAM BHIM SOREN <35399866+SSBsoren@users.noreply.github.com>
2020-07-21 13:00:12 +0530

    Flutter_hello
    basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,42 @@
 import 'package:flutter/material.dart';
 
-
-void main(){
-  runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Hello Sagen',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Sagen Soren'),
-        ),
-        body: HelloSagen(),
-      ),
-    ),
-  );
+void main() {
+  runApp(MaterialApp(
+    title: "Hello App",
+    home: HomePage(),
+  ));
 }
 
-class HelloSagen extends StatelessWidget{
+class HomePage extends StatelessWidget {
   @override
-  Widget build(BuildContext context){
-    return Center(
-      child: Container(
-        color: Colors.greenAccent,
-        height: 400.0,
-        width: 300.0,
-
-        child: Center(
-          child: Text(
-            'Hello Sagen',
-            style: TextStyle(fontSize: 40.0),
-            textAlign: TextAlign.center,
-          ),
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Hello App"),
         ),
-      ),
-    );
-  }
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8.0),
+            alignment: Alignment.center,
+            width: 100.0,
+            height: 100.0,
+            child: Text("Sagen Soren",style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 13.0,
+            ),),
 
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey[400],
+                    blurRadius: 5.0,
+                    offset: Offset(2.0, 5.0)
+                  )
+                ],
+                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+          ),
+        ));
+  }
 }
